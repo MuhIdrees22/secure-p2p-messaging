90bfc072863d8a72ec7a10d81bebf51da0307fda
MuhIdrees22 <muhammad.idrees1@ontariotechu.net>
2026-03-15 14:49:56 -0400

    Implement relay server for forwarding messages

--- server/relay_server.py
+++ server/relay_server.py
@@ -1,1 +1,44 @@
 # Relay server for forwarding encrypted messages
+import socket
+import threading
+
+HOST = '127.0.0.1'
+PORT = 5555
+
+clients = []
+
+def handle_client(client_socket):
+    while True:
+        try:
+            message = client_socket.recv(1024)
+            if not message:
+                break
+
+            # forward message to all other clients
+            for client in clients:
+                if client != client_socket:
+                    client.send(message)
+
+        except:
+            break
+
+    clients.remove(client_socket)
+    client_socket.close()
+
+def start_server():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((HOST, PORT))
+    server.listen()
+
+    print(f"Server running on {HOST}:{PORT}")
+
+    while True:
+        client_socket, addr = server.accept()
+        print(f"New connection from {addr}")
+
+        clients.append(client_socket)
+
+        thread = threading.Thread(target=handle_client, args=(client_socket,))
+        thread.start()
+
+start_server()
